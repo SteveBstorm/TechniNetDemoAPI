a2ba5f0a27b1c1d66ba4da010fd8f772f8215ebc
steve <steve.lorent@bstorm.be>
2023-10-05 13:48:35 +0200

    Ajoutez des fichiers projet.

---
+++ TechniNetDemoAPI/Controllers/PersonController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechniNetDemoAPI.Models;
+
+namespace TechniNetDemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(Person.liste);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Person person)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest();
+
+            if (Person.liste.Where(x => x.Firstname == person.Firstname).Count() > 0)
+            {
+                return BadRequest("La personne existe déjà");
+            }
+            Person.Ajout(person);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            Person p = Person.liste.Where(x => x.Id == id).First();
+            
+            return Ok(p);
+        }
+
+        [HttpGet("byname/{name}")]
+        public IActionResult GetById([FromRoute] string name)
+        {
+            Person p = Person.liste.Where(x => x.Firstname == name).First();
+
+            return Ok(p);
+        }
+    }
+}
---
+++ TechniNetDemoAPI/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechniNetDemoAPI.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Firstname { get; set; }
+        [MinLength(5)]
+        public string Lastname { get; set; }
+
+        public static List<Person> liste = new List<Person>()
+        {
+            new Person { Id = 1, Firstname = "Arthur", Lastname = "Pendragon" },
+            new Person { Id = 2, Firstname = "Merlin", Lastname = "L'enchanteur" },
+            new Person { Id = 3, Firstname = "Léodagan", Lastname = "De Carmélide" } 
+        };
+
+        public static void Ajout(Person p) { liste.Add(p); }
+    }
+}
